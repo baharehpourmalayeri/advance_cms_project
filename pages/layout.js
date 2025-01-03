2ec0539021d38da8afaeac0af375ee16bfbe3818
baharehpourmalayeri <baharehpoormalayeri@gmail.com>
2025-01-03 12:42:38 +0100

    getting the menu dynamically from Contentful

--- pages/layout.js
+++ pages/layout.js
@@ -1,9 +1,26 @@
 import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 import "./css/globals.css";
+import { useEffect, useState } from "react";
+import { fetchContent } from "@/lib/contentful";
 
 export default function Layout({ children }) {
+  const [menu, setMenu] = useState([]);
+
+  const fetchMenu = async () => {
+    try {
+      const menuData = await fetchContent("menu");
+      setMenu(menuData);
+    } catch (error) {
+      console.error("Error fetching menu data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,18 +31,17 @@ export default function Layout({ children }) {
         <nav className="container">
           <h3>BP</h3>
           <ul className="nav">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/project-index">Projects</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
+            {menu && menu.length > 0 ? (
+              menu
+                .sort((a, b) => a.fields.order - b.fields.order)
+                .map((item, index) => (
+                  <li key={index}>
+                    <a href={item.fields.link}>{item.fields.title}</a>
+                  </li>
+                ))
+            ) : (
+              <li>No menu items available</li>
+            )}
           </ul>
         </nav>
         <div className="main-content">{children}</div>
